75a93b913483f717c17eb9941defe7a10f46ccc2
Will Hurley <wjhurley@comcast.net>
2016-09-12 01:13:20 -0700

    Added missing algorithm from Beginner Alogorithm Scripting and reordered to match FCC

--- algorithms.js
+++ algorithms.js
@@ -12,6 +12,57 @@ function fibonacci(n) {
   }
   return intFib;
 }
+/*Given an array of objects (contacts), determine if the contact (firstName) exists and if so, determine if the property (prop) exists. If either does not exist, return "No such ..."*/
+function lookUpProfile(firstName, prop){
+  for (i=0; i<contacts.length; i++) {
+    if (contacts[i].firstName != firstName) {
+      continue;
+    } else {
+      if (contacts[i][prop]) {
+        return contacts[i][prop];
+      } else {
+        return "No such property";
+      }
+    }
+  }
+  return "No such contact";
+}
+/*Reverses provided string*/
+function reverseString(str) {
+  var arrStr = str.split("");
+  var strRev = arrStr.reverse().join("");
+  return strRev;
+}
+/*Returns factorial of integer provided*/
+function factorialize(num) {
+  var intTotal = 1;
+  for(var i = 1; i <= num; i++) {
+    intTotal *= i;
+  }
+  return intTotal;
+}
+/*Checks to see if the provided string is a palindrome, regardless of special characters and case*/
+function palindrome(str) {
+  var regNonChar = /\W+/g;
+  var strNew = str.replace(regNonChar, "").toLowerCase();
+  strNew = strNew.replace("_","");
+  var strRev = strNew.split("").reverse().join("");
+  if (strNew === strRev) {
+    return true;
+  } else {
+    return false;
+  }
+}
+/*When provided a string of words, this function will return the length of the longest word*/
+function findLongestWord(str) {
+  var arrStr = str.split(" ");
+  var arrStrLength = [];
+  for (i=0; i<arrStr.length; i++) {
+    arrStrLength[i] = arrStr[i].length;
+  }
+  arrStrLength.sort(function(a,b){return b-a;});
+  return arrStrLength[0];
+}
 /*When provided a string of words, regardless of capitalization, will return the same string with only the first character in each word capitalized.*/
 function titleCase(str) {
   var arrStr = str.toLowerCase().split(" ");
@@ -26,36 +77,6 @@ function titleCase(str) {
   }
   arrStr = arrNewWord.join(" ");
   return arrStr;
-}
-/*When provided a string of words, this function will return the length of the longest word*/
-function findLongestWord(str) {
-  var arrStr = str.split(" ");
-  var arrStrLength = [];
-  for (i=0; i<arrStr.length; i++) {
-    arrStrLength[i] = arrStr[i].length;
-  }
-  arrStrLength.sort(function(a,b){return b-a;});
-  return arrStrLength[0];
-}
-/*Checks to see if the provided string is a palindrome, regardless of special characters and case*/
-function palindrome(str) {
-  var regNonChar = /\W+/g;
-  var strNew = str.replace(regNonChar, "").toLowerCase();
-  strNew = strNew.replace("_","");
-  var strRev = strNew.split("").reverse().join("");
-  if (strNew === strRev) {
-    return true;
-  } else {
-    return false;
-  }
-}
-/*given a number, returns the factorial (i.e. 5 returns 1*2*3*4*5)*/
-function factorialize(num) {
-  var total = 1;
-  for (i=1;i<=num;i++) {
-    total *= i;
-  }
-  return total;
 }
 /*given a set of numerical arrays, returns the highest value from each array in an array*/
 function largestOfFour(arr) {
@@ -107,21 +128,6 @@ function chunkArrayInGroups(arr, size) {
     arrGroups.push(arr.slice(i,(i+size)));
   }
   return arrGroups;
-}
-/*Given an array of objects (contacts), determine if the contact (firstName) exists and if so, determine if the property (prop) exists. If either does not exist, return "No such ..."*/
-function lookUpProfile(firstName, prop){
-  for (i=0; i<contacts.length; i++) {
-    if (contacts[i].firstName != firstName) {
-      continue;
-    } else {
-      if (contacts[i][prop]) {
-        return contacts[i][prop];
-      } else {
-        return "No such property";
-      }
-    }
-  }
-  return "No such contact";
 }
 /*Given an array and an integer howMany, return the remaining elements after removing howMany elements*/
 function slasher(arr, howMany) {
