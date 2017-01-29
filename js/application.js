0f2a8decb288ec1d832113755ef8a8b031a87208
Will Hurley <wjhurley@comcast.net>
2017-01-28 17:46:54 -0800

    adds var declaration in application.js

--- js/application.js
+++ js/application.js
@@ -1,5 +1,5 @@
 function convert() {
-  intDecimal = document.getElementById("decimalNum").value;
-  lblRomanNum = document.getElementById("romanNum");
+  var intDecimal = document.getElementById("decimalNum").value;
+  var lblRomanNum = document.getElementById("romanNum");
   lblRomanNum.innerHTML = convertToRoman(intDecimal);
 }
