be5b6c9d6aae0e8f51a5f4dbde92b242e00f3360
Will Hurley <wjhurley@comcast.net>
2017-01-29 02:17:17 -0800

    adds Exact Change algorithm

--- js/algorithms.js
+++ js/algorithms.js
@@ -692,3 +692,47 @@ function sym(args) {
   }
   return arrSymDiffOne;
 }
+/*EXACT CHANGE - Given an amount owed, cash provided, and cash in the register, return the change owed in each denomination*/
+function checkCashRegister(price, cash, cid) {
+  var denominations = [
+    {name: "ONE HUNDRED", val: 100.00},
+    {name: "TWENTY", val: 20.00},
+    {name: "TEN", val: 10.00},
+    {name: "FIVE", val: 5.00},
+    {name: "ONE", val: 1.00},
+    {name: "QUARTER", val: 0.25},
+    {name: "DIME", val: 0.10},
+    {name: "NICKEL", val: 0.05},
+    {name: "PENNY", val: 0.01}
+  ];
+  var change = cash - price;
+  var register = cid.reduce(function(acc, curr) {
+    acc.total += curr[1];
+    acc[curr[0]] = curr[1];
+    return acc;
+  }, {total: 0});
+  register.total = Number(register.total.toFixed(2));
+  if(register.total === change) {
+    return "Closed";
+  } else if(register.total < change) {
+    return "Insufficient Funds";
+  }
+  var arrChange = denominations.reduce(function(acc, curr) {
+    var value = 0;
+    while(register[curr.name] > 0 && change >= curr.val) {
+      change -= curr.val;
+      register[curr.name] -= curr.val;
+      value += curr.val;
+      console.log(change);
+      change = Math.round(change * 100) / 100;
+    }
+    if(value > 0) {
+      acc.push([curr.name, value]);
+    }
+    return acc;
+  }, []);
+  if(arrChange.length < 1 || change > 0) {
+    return "Insufficient Funds";
+  }
+  return arrChange;
+}
