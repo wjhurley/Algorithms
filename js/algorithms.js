b9b818d10a6b457e2585a6ab1019981cb7db3817
whurley <whurley@velocity.org>
2021-05-18 23:26:12 -0700

    Add coding challenge from interview

--- js/algorithms.js
+++ js/algorithms.js
@@ -1,4 +1,4 @@
-/*NOT A CHALLENGE ON FCC - Fibonacci sequence that starts with 0 as the first number in the sequence (i.e. 0,1,1...)*/
+// NOT A CHALLENGE ON FCC - Fibonacci sequence that starts with 0 as the first number in the sequence (i.e. 0,1,1...)
 function fibonacci(n) {
     let intA = 0;
     let intB = 1;
@@ -17,4 +17,43 @@ function fibonacci(n) {
     }
 
     return intFib;
+}
+// Coding challenge from interview on 5/17/21 - encrypt and decrypt UDP payloads
+class Payload {
+    constructor() {
+        this.seed = parseInt('0xEC', 16);
+    }
+
+    static encrypt(str) {
+        const encoder = new TextEncoder();
+        const encodedArray = encoder.encode(str);
+        const encryptedArray = new Uint8Array(encodedArray);
+
+        for (const [index, val] of encodedArray.entries()) {
+            if (index === 0) {
+                encryptedArray[index] = val ^ this.seed;
+                continue;
+            }
+
+            encryptedArray[index] = encryptedArray[index - 1] ^ val;
+        }
+
+        return encryptedArray;
+    }
+
+    static decrypt(uint) {
+        const decoder = new TextDecoder();
+        const encodedArray = new Uint8Array(uint);
+
+        for (const [index, val] of uint.entries()) {
+            if (index === 0) {
+                encodedArray[index] = val ^ this.seed;
+                continue;
+            }
+
+            encodedArray[index] = uint[index - 1] ^ val;
+        }
+
+        return decoder.decode(encodedArray);
+    }
 }
